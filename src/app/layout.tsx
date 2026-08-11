2d38f7d812d6a5c555acd95a52bc0be8d0dcda8d
vananawani <nanamatsumoto7326@gmail.com>
2026-08-11 18:37:48 +0900

    style: update favicon image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
 	return (
 		<html lang="ja">
 			<head>
-				<link rel="icon" href="/favicon.svg" type="image/svg+xml"></link>
+				<link rel="icon" href="/favicon.png" type="image/png"></link>
 			</head>
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
 		</html>
